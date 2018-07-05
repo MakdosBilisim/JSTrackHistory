be2f4b019d3e74c8e691f3df7c3ef2bc5a6a8616
Makdos Bilişim Teknolojileri <admin@makdos.com>
2018-07-05 12:14:59 +0300

    Bazı linkler dahil edilmek istenmezse
    Bazı linkler takip edilmek istenmez ise if (!gidilecek_link.include("giris")) gibi

--- trackhistory.js
+++ trackhistory.js
@@ -1,35 +1,31 @@
-<script>
+        window.onclick = function (e) {
 
-    window.onclick = function (e) {
-
-        var node = e.target;
-        while (node != undefined && node.localName != 'a') {
-            node = node.parentNode;
-        }
-
-        if (node != undefined) {
-
-            var onceki_link = document.referrer;
-            var gidilecek_link = node.href;
-
-            var gidilecek_link_domaini_bul = gidilecek_link.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
-            var gidilecek_link_domain = gidilecek_link_domaini_bul && gidilecek_link_domaini_bul[1];
-            var gidilecek_link_domainsiz = gidilecek_link.replace(gidilecek_link_domain, '').replace(/(^\w+:|^)\/\//, '');
-
-            if (onceki_link.includes(gidilecek_link_domainsiz)) {
-                window.history.back();
+            var node = e.target;
+            while (node != undefined && node.localName != 'a') {
+                node = node.parentNode;
             }
-            else {
-                window.location.href = gidilecek_link;
+
+            if (node != undefined) {
+
+                var onceki_link = document.referrer;
+                var gidilecek_link = node.href;
+
+                if (!gidilecek_link.include("giris")) {
+
+                    var gidilecek_link_domaini_bul = gidilecek_link.match(/^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)/i);
+                    var gidilecek_link_domain = gidilecek_link_domaini_bul && gidilecek_link_domaini_bul[1];
+                    var gidilecek_link_domainsiz = gidilecek_link.replace(gidilecek_link_domain, '').replace(/(^\w+:|^)\/\//, '');
+
+                    if (onceki_link.includes(gidilecek_link_domainsiz)) {
+                        window.history.back();
+                    }
+                    else {
+                        window.location.href = gidilecek_link;
+                    }
+                    return false;
+
+                }
+            } else {
+                return true;
             }
-            return false;
-        } else {
-            return true;
-        }
-    };
-
-
-    var ex = history.length;
-    console.log(ex + " link gezilerek bu sayfaya ulaşıldı!.")
-
-</script>
+        };
